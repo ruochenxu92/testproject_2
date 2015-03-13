4f9984731250d12b5b90c71288ef45fdd37f0f34
xxu46 <xxu46@illinois.edu>
2015-03-13 17:22:16 -0500

    Achieve all

--- task/models.py
+++ task/models.py
@@ -4,6 +4,9 @@ from django.contrib.auth.models import User
 from django.utils.encoding import smart_unicode
 import datetime
 
+
+
+
 class Task (models.Model):
     name = models.CharField(max_length=30)
 
